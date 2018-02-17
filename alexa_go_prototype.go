a35ab4d79ac9fc995734fbea66d870efdf06aeaa
Ben Rockwood <benr@cuddletech.com>
2018-02-17 15:59:24 -0800

    Add useful comments

--- alexa_go_prototype.go
+++ alexa_go_prototype.go
@@ -7,19 +7,24 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// This "void" type acts as a holder to unmarshal the input JSON into
 type Event interface{}
 
 func HandleRequest(ctx context.Context, e Event) (string, error) {
-	//log.Printf("Got event: %v+\n", e)
+	// Output the raw unindexed map[interface{}]interface{}:
+	//log.Printf("Raw event: %v+\n", e)
 
+	// Use Type Assertion to return an indexed map:
 	myEvent := e.(map[string]interface{})
 
+	// Iterate over Map Keys to examine input event:
 	fmt.Println("---- Dumping Events Map: ----")
 	for k, v := range myEvent {
 		fmt.Printf("Key %v = %v\n", k, v)
 	}
 	fmt.Println("---- Done. ----")
 
+	// Example of accessing map value via index:
 	//fmt.Println("Key1 is ", myEvent["key1"])
 
 	return fmt.Sprintf("Done!"), nil
